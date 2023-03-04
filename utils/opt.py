694f916d0a1ad251a3d5f36a06bf48f6a8c249f2
Shazna Khanom <shaznakhanom4@gmail.com>
2023-03-04 15:36:49 +0000

    Added gamma option for lr_scheduler

--- utils/opt.py
+++ utils/opt.py
@@ -19,17 +19,16 @@ class Options:
         self.parser.add_argument('--datetime', type=str, default=curr_time, help='datatime now')
         self.parser.add_argument('--raw_data_path', type=str, default='data/EC3D/EC3D.pickle')
         self.parser.add_argument('--processed_path', type=str, default='data/EC3D/tmp_wo_val.pickle')
+        self.parser.add_argument('--record', action='store_true', help='Record with Tensorboard, use --record to enable')
 
         # Model and Running Options
-
-        # Model and Running Options
-        # parser.add_argument('--device', type=str, default=device, help='Device used for model training')
         self.parser.add_argument('--epoch', type=int, default=50, help='Number of epochs')
         self.parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
         self.parser.add_argument('--hidden', type=int, default=256, help='Number of hidden features')
         self.parser.add_argument('--p_dropout', type=float, default=0.5, help='Dropout probability, 1 for none')
         self.parser.add_argument('--lr', type=float, default=0.01, help='Learning rate')
         self.parser.add_argument('--lr_decay', type=int, default=5, help='every lr_decay epoch do lr decay')
+        self.parser.add_argument('--gamma', type=float, default=0.1)
     def _print(self):
         print("\n==================Options=================")
         pprint(vars(self.arg), indent=4)
