13f61f8dec5037d850e7bc8a94703bbbc8f68a65
Shazna Khanom <shaznakhanom4@gmail.com>
2023-03-07 02:02:22 +0000

    New arguments: gamma, step size and record

--- utils/opt.py
+++ utils/opt.py
@@ -13,13 +13,13 @@ class Options:
 
     def _initial(self):
         curr_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         # General options
         self.parser.add_argument('--datetime', type=str, default=curr_time, help='datatime now')
         self.parser.add_argument('--raw_data_path', type=str, default='data/EC3D/EC3D.pickle')
         self.parser.add_argument('--processed_path', type=str, default='data/EC3D/tmp_wo_val.pickle')
         self.parser.add_argument('--record', action='store_true', help='Record with Tensorboard, use --record to enable')
+        self.parser.add_argument('--note', type=str, default='', help='A note about the run')
 
         # Model and Running Options
         self.parser.add_argument('--epoch', type=int, default=50, help='Number of epochs')
@@ -28,7 +28,9 @@ class Options:
         self.parser.add_argument('--p_dropout', type=float, default=0.5, help='Dropout probability, 1 for none')
         self.parser.add_argument('--lr', type=float, default=0.01, help='Learning rate')
         self.parser.add_argument('--lr_decay', type=int, default=5, help='every lr_decay epoch do lr decay')
-        self.parser.add_argument('--gamma', type=float, default=0.1)
+        self.parser.add_argument('--gamma', type=float, default=0.2)
+        self.parser.add_argument('--step_size', type=int, default=10, help='learning rate for the lr_scheduler')
+
     def _print(self):
         print("\n==================Options=================")
         pprint(vars(self.arg), indent=4)
@@ -42,4 +44,5 @@ class Options:
 
 
 if __name__ == '__main__':
+
     arg = Options().parse()
