5e82535db4d1c0f5ccd58958b1cbee57eb28fbdb
Shazna Khanom <shaznakhanom4@gmail.com>
2023-04-23 15:52:30 +0100

    dtw_loss_v2 removed (not used)

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-from dataset import EC3D, EC3D_new
+from dataset import EC3D_V1, EC3D_V2
 
 """
     Functions for training: get_labels, dtw_loss
@@ -15,7 +15,7 @@ from dataset import EC3D, EC3D_new
 
 
 def load_3d():
-    temp_path = 'data/EC3D/tmp_3d.pickle'
+    temp_path = 'data/EC3D/tmp_3d_1.pickle'
     raw_data_path = 'data/EC3D/EC3D.pickle'
     is_cuda = torch.cuda.is_available()
     try:
@@ -27,12 +27,10 @@ def load_3d():
 
     except FileNotFoundError:
         print('File Not Found: Processing raw data...')
-        sets = [[0, 1, 2], [], [3]]
+        test_subject = 3
         is_cuda = torch.cuda.is_available()
-        data_train = EC3D_new(raw_data_path, sets=sets, split=0, rep_3d=True, normalization='sample',
-                              is_cuda=is_cuda)
-        data_test = EC3D_new(raw_data_path, sets=sets, split=2, rep_3d=True, normalization='sample',
-                             is_cuda=is_cuda)
+        data_train = EC3D_V2(raw_data_path, data_type='DCT', test_subject=test_subject, is_cuda=is_cuda)
+        data_test = EC3D_V2(raw_data_path, data_type='DCT', sets=[[test_subject]], is_cuda=is_cuda, test=True)
         print('Writing processed data...')
         with open(temp_path, 'wb') as f:
             pickle.dump({'train': data_train, 'test': data_test}, f)
@@ -42,7 +40,8 @@ def load_3d():
 
 
 def load_DCT():
-    temp_path = 'data/EC3D/tmp_DCT_3CH.pickle'
+    # temp_path = 'data/EC3D/tmp_DCT_3CH.pickle'
+    temp_path = 'data/EC3D/tmp_DCT.pickle'
     raw_data_path = 'data/EC3D/EC3D.pickle'
     is_cuda = torch.cuda.is_available()
     try:
@@ -54,12 +53,10 @@ def load_DCT():
 
     except FileNotFoundError:
         print('File Not Found: Processing raw data...')
-        sets = [[0, 1, 2], [], [3]]
-        is_cuda = torch.cuda.is_available()
-        data_train = EC3D_new(raw_data_path, sets=sets, split=0, rep_dct=3, rep_3d=False, normalization='sample',
-                              is_cuda=is_cuda)
-        data_test = EC3D_new(raw_data_path, sets=sets, split=2, rep_dct=3, rep_3d=False, normalization='sample',
-                             is_cuda=is_cuda)
+        # sets = [[0, 1, 2], [], [3]]
+        test_subject = 3
+        data_train = EC3D_V2(raw_data_path, data_type='DCT', test_subject=test_subject, is_cuda=is_cuda)
+        data_test = EC3D_V2(raw_data_path, data_type='DCT', sets=[[test_subject]], is_cuda=is_cuda, test=True)
         print('Writing processed data...')
         with open(temp_path, 'wb') as f:
             pickle.dump({'train': data_train, 'test': data_test}, f)
@@ -81,10 +78,10 @@ def load_original():
 
     except FileNotFoundError:
         print('Processing raw data.')
-        sets = [[0, 1, 2], [], [3]]
+        test_subject = 3
         is_cuda = torch.cuda.is_available()
-        data_train = EC3D(raw_data_path, sets=sets, split=0, is_cuda=is_cuda)
-        data_test = EC3D(raw_data_path, sets=sets, split=2, is_cuda=is_cuda)
+        data_train = EC3D_V1(raw_data_path, test_subject=test_subject, is_cuda=is_cuda)
+        data_test = EC3D_V1(raw_data_path, sets=[[test_subject]], is_cuda=is_cuda, test=True)
         with open(temp_path, 'wb') as f:
             pickle.dump({'train': data_train, 'test': data_test}, f)
 
@@ -110,36 +107,18 @@ def get_labels(raw_labels, level=0):
         return torch.from_numpy(labels).long()
 
 
-def dtw_loss_v2(predictions, targets, criterion, is_cuda=False):
-    loss = 0
-    for i, target in enumerate(targets):
-        prediction = predictions[i]
-        if isinstance(prediction, np.ndarray):
-            prediction = torch.from_numpy(prediction)
-        if isinstance(target, np.ndarray):
-            target = torch.from_numpy(target)
-        # p0 = (1, frames, 19*3 dims)
-        out = prediction.unsqueeze(0)
-        targ = target.reshape(target.size(0), target.size(1)*target.size(2)).unsqueeze(0)
-        out = out.cuda()
-        targ = targ.cuda()
-        crit = criterion(out, targ) - 1 / 2 * (criterion(out, out) + criterion(targ, targ))
-        loss += crit
-
-    return loss
-
-
 def dtw_loss(originals, deltas, targets, criterion, attentions=None, is_cuda=False, test=False):
     loss = 0
     preds = []
     dtw_loss_corr = []
     dtw_loss_org = []
     for i, o in enumerate(originals):
-
+        # o.shape = [57, len], org.shape and targ.shape = [1, seq_len, 57]
         length = o.shape[1]
         org = torch.from_numpy(o).T.unsqueeze(0)
         targ = torch.from_numpy(targets[i]).T.unsqueeze(0)
 
+        # deltas[i].shape = [57, 25]
         if length > deltas[i].shape[1]:
             m = torch.nn.ZeroPad2d((0, length - deltas[i].shape[1], 0, 0))
             # delt = dct.idct_2d(m(deltas[i]).T.unsqueeze(0))
@@ -151,13 +130,16 @@ def dtw_loss(originals, deltas, targets, criterion, attentions=None, is_cuda=Fal
         if attentions is not None:
             delt = torch.mul(delt, attentions[i].T.unsqueeze(0))
 
+        # delt.shape = [1, seq_len, 57]
         out = org.cuda() + delt.cuda()
+        # out = delt.cuda()
 
         if is_cuda:
             out = out.cuda()
             targ = targ.cuda()
 
         crit = criterion(out, targ) - 1 / 2 * (criterion(out, out) + criterion(targ, targ))
+        normalized_crit = crit / length
         crit_org = criterion(org.cuda(), targ) - 1 / 2 * (criterion(org.cuda(), org.cuda()) + criterion(targ, targ))
         mse = torch.nn.MSELoss()
         smoothness_loss = mse(out[:, 1:], out[:, :-1])
@@ -166,6 +148,7 @@ def dtw_loss(originals, deltas, targets, criterion, attentions=None, is_cuda=Fal
         dtw_loss_org.append(crit_org.item())
         loss += crit + 1e-3 * smoothness_loss  # dtw_loss + smoothness
         # loss += crit  # without smoothness
+        # loss += normalized_crit + 1e-3 * smoothness_loss
 
         if test:
             preds.append(out[0].detach().cpu().numpy().T)
@@ -175,24 +158,10 @@ def dtw_loss(originals, deltas, targets, criterion, attentions=None, is_cuda=Fal
     else:
         return loss
 
-
-if __name__ == '__main__':
-    pass
-    # targets shape = list[3, 19, 24]
-    targets = [np.random.rand(3, 19, 24) for _ in range(32)]
-    # predictions shape = [32, 3, 19, 24]
-    predictions = np.random.rand(32, 3, 19, 24)
-    from softdtw import SoftDTW
-
-    is_cuda = torch.cuda.is_available()
-    criterion = SoftDTW(use_cuda=is_cuda, gamma=0.01)
-
-    dtw_loss_v2(predictions, targets, criterion, is_cuda=is_cuda)
-
 def nopeak_mask(size):
     np_mask = np.triu(np.ones((1, size, size)),
-    k=1).astype('uint8')
-    np_mask =  Variable(torch.from_numpy(np_mask) == 0)
+                      k=1).astype('uint8')
+    np_mask = Variable(torch.from_numpy(np_mask) == 0)
     return np_mask.to()
 
 
